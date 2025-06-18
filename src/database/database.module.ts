a739a26d52be6792d2155cae120f14c890ee3d6e
Kim Jin Tae <realbig4199@gmail.com>
2025-06-18 21:14:05 +0900

    refactor: 데이터베이스 설정에 ConfigService 적용

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,20 +11,23 @@ import { DatabaseService } from './database.service';
   imports: [
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: async () => ({
-        type: process.env.DB_TYPE as 'mysql',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        synchronize: false,
-        entities: [path.join(__dirname, 'entity', '*.entity.{ts,js}')],
-        migrations: [path.join(__dirname, 'migrations', '*.{ts,js}')],
-        migrationsRun: true,
-        migrationsTableName: 'migrations_history',
-        // logging: true, // 트랜잭션 확인용
-      }),
+      useFactory: async (config: ConfigService) => {
+        const dbConfig = config.get('db');
+
+        return {
+          type: dbConfig.type,
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.database,
+          synchronize: false,
+          entities: [path.join(__dirname, 'entity', '*.entity.{ts,js}')],
+          migrations: [path.join(__dirname, 'migrations', '*.{ts,js}')],
+          migrationsRun: true,
+          migrationsTableName: 'migrations_history',
+        };
+      },
       dataSourceFactory: async (options) => {
         const dataSource = new DataSource(options);
         await dataSource.initialize();
