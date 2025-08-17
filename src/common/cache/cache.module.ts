b5b284de404f5d7897cc1d83d8bfe6bc21587260
lilyshin <lilyshin.dev@gmail.com>
2025-08-17 21:25:36 +0900

    feat: redis cache manager 버전 다운그레이드

--- src/common/cache/cache.module.ts
+++ src/common/cache/cache.module.ts
@@ -1,9 +1,27 @@
+import { CacheModule as NestCacheModule } from '@nestjs/cache-manager';
 import { Global, Module } from '@nestjs/common';
+import { redisStore } from 'cache-manager-redis-yet';
 
 import { CacheService } from '@/common/cache/cache.service';
+import { ConfigService } from '@/config/config.service';
 
 @Global()
 @Module({
+  imports: [
+    NestCacheModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        const redisConfig = config.get('redis');
+
+        return {
+          store: await redisStore({
+            url: redisConfig.url,
+            ttl: redisConfig.options.ttl,
+          }),
+        };
+      },
+    }),
+  ],
   providers: [CacheService],
   exports: [CacheService],
 })
