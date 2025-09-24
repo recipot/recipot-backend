45a3cf9a815760669e970a0017fd023c74482568
lilyshin <lilyshin.dev@gmail.com>
2025-09-25 04:15:25 +0900

    feat:  UserModule 의존성 주입 문제 해결

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -1,17 +1,19 @@
 import { AuthModule } from '@/api/auth/auth.module';
 import { DatabaseModule } from '@/database/database.module';
+import { CommonCode } from '@/database/entity/common-code.entity';
+import { Recipe } from '@/database/entity/recipe.entity';
+import { UserRecipeBookmark } from '@/database/entity/user-recipe-bookmark.entity';
 import { User } from '@/database/entity/user.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRecipeBookmarkCustomRepository } from './user-recipe-bookmark.custom-repository';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { CommonCode } from '@/database/entity/common-code.entity';
 
 @Module({
   imports: [
     DatabaseModule,
-    TypeOrmModule.forFeature([User, CommonCode]),
+    TypeOrmModule.forFeature([User, UserRecipeBookmark, Recipe, CommonCode]),
     AuthModule,
   ],
   controllers: [UserController],
