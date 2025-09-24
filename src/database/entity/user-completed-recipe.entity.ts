eee88ed3609279af9e246fe529bbcc57107c763e
lilyshin <lilyshin.dev@gmail.com>
2025-09-25 05:32:26 +0900

    feat: 요리 완료 Entity 생성

---
+++ src/database/entity/user-completed-recipe.entity.ts
@@ -0,0 +1,43 @@
+import { Column, Entity } from 'typeorm';
+import { CommonEntity } from './common.entity';
+
+@Entity('user_completed_recipes')
+export class UserCompletedRecipe extends CommonEntity {
+  @Column({
+    type: 'int',
+    name: 'user_id',
+    comment: '유저 PK',
+  })
+  userId: number;
+
+  @Column({
+    type: 'int',
+    name: 'recipe_id',
+    comment: '레시피 PK',
+  })
+  recipeId: number;
+
+  @Column({
+    type: 'boolean',
+    default: false,
+    name: 'is_completed',
+    comment: '완료 여부',
+  })
+  isCompleted: boolean;
+
+  @Column({
+    type: 'datetime',
+    nullable: true,
+    name: 'completed_at',
+    comment: '완료 날짜',
+  })
+  completedAt: Date;
+
+  @Column({
+    type: 'boolean',
+    default: false,
+    name: 'is_reviewed',
+    comment: '후기 작성 여부',
+  })
+  isReviewed: boolean;
+}
