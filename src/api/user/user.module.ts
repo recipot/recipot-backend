77c04d16fb7dc5bdef8881d99785c94d4ae3dd61
lilyshin <105103355+lilyshin@users.noreply.github.com>
2025-09-25 04:33:08 +0900

    feat: 레시피 entity 조리난이도, 조리방식 추가 (#42)
    * feat: 사용자별 레시피 북마크 조회/추가/삭제 구현
    
    * test: 사용자 북마크 API 테스트코드 추가
    
    * feat: 북마크 추가 시 레시피 존재 여부 확인 로직 추가
    
    * feat: customRepository query alias 추가
    
    * feat: 북마크 조회 시 필요한 컬럼만 반환, 레시피 타이틀 컬럼 추가
    
    * feat: 불필요한 주석 제거
    
    * refactor: 북마크 생성 API 리팩토링
    
    * feat: package-lock.json rollback
    
    * feat: package-lock.json rollback
    
    * feat:  UserModule 의존성 주입 문제 해결
    
    * fix: build error 수정

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -1,14 +1,14 @@
 import { AuthModule } from '@/api/auth/auth.module';
 import { DatabaseModule } from '@/database/database.module';
-import { User } from '@/database/entity/user.entity';
-import { UserRecipeBookmark } from '@/database/entity/user-recipe-bookmark.entity';
+import { CommonCode } from '@/database/entity/common-code.entity';
 import { Recipe } from '@/database/entity/recipe.entity';
+import { UserRecipeBookmark } from '@/database/entity/user-recipe-bookmark.entity';
+import { User } from '@/database/entity/user.entity';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRecipeBookmarkCustomRepository } from './user-recipe-bookmark.custom-repository';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { CommonCode } from '@/database/entity/common-code.entity';
 
 @Module({
   imports: [
