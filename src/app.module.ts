f6a11cb4103228b81c24417ada22a9d7dc5bd636
Kim Jin Tae <realbig4199@gmail.com>
2025-10-19 17:21:59 +0900

    refactor: 모듈 파일명 오타 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { SeasoningModule } from './api/seasoning/seasoning.module';
 import { ToolModule } from './api/tool/tool.module';
 import { UploadModule } from './api/upload/upload.module';
 import { ResponseTimeInterceptor } from './common/interceptors/response-time.interceptor';
-import { MeasurementGuideModule } from './api/measurement-guide/mesurement-guide.module';
+import { MeasurementGuideModule } from './api/measurement-guide/measurement-guide.module';
 
 export const FeatureModules = [
   UserModule,
