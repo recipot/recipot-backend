113c3a4dedfea5552c7c52a45a26679c7be9d20c
Kim Jin Tae <realbig4199@gmail.com>
2025-11-02 22:49:30 +0900

    feat: 불필요한 시딩 파일 제거

--- src/database/seeds/index.ts
+++ src/database/seeds/index.ts
@@ -1,11 +1,7 @@
 import { DataSource } from 'typeorm';
 import { CommonCodeSeed } from './common-code.seed';
 import { UserSeed } from './user.seed';
-import { IngredientSeed } from './ingredient.seed';
-import { SeasoningSeed } from './seasoning.seed';
-import { ToolSeed } from './tool.seed';
 import { ConditionSeed } from './condition.seed';
-import { IngredientRestrictedSeed } from './ingredient-restricted.seed';
 
 export class DatabaseSeeder {
   constructor(
@@ -24,24 +20,6 @@ export class DatabaseSeeder {
     const userSeed = new UserSeed(this.dataSource);
     await userSeed.run();
 
-    // IngredientSeed 실행
-    const ingredientSeed = new IngredientSeed(this.dataSource);
-    await ingredientSeed.run();
-
-    // IngredientRestrictedSeed
-    const ingredientRestrictedSeed = new IngredientRestrictedSeed(
-      this.dataSource,
-    );
-    await ingredientRestrictedSeed.run();
-
-    // SeasoningSeed 실행
-    const seasoningSeed = new SeasoningSeed(this.dataSource);
-    await seasoningSeed.run();
-
-    // ToolSeed 실행
-    const toolSeed = new ToolSeed(this.dataSource);
-    await toolSeed.run();
-
     // ConditionSeed 실행
     const conditionSeed = new ConditionSeed(this.dataSource);
     await conditionSeed.run();
@@ -55,4 +33,4 @@ export class DatabaseSeeder {
 }
 
 // 개별 seeder들도 export
-export { CommonCodeSeed, UserSeed, IngredientSeed, ConditionSeed };
+export { CommonCodeSeed, UserSeed, ConditionSeed };
